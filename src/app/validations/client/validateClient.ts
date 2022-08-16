8d8320a7e85818c84d3628cddcd5e38dc6a400f0
paulorayann <paulorpsena@gmail.com>
2022-08-16 15:59:24 -0300

    feat: add client Joi post method validation

---
+++ src/app/validations/client/validateClient.ts
@@ -0,0 +1,49 @@
+import { Request, Response, NextFunction } from 'express'
+import JoiImport from 'joi'
+import DateExtension from '@joi/date'
+import cpfValidation from '../../utils/cpfValidation'
+import {cpfValid}  from '../../utils/regex'
+
+const Joi = JoiImport.extend(DateExtension) as typeof JoiImport
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const client = Joi.object({
+      name: Joi.string().trim().required(),
+      cpf: Joi.string()
+      .trim()
+      .min(11)
+      .max(14)
+      .regex(cpfValid)
+      .message('The CPF field has an invalid format, please try XXX.XXX.XXX-XX and use numbers only')
+      .custom((cpf) => {
+        if (!cpfValidation(cpf)) throw Error('Please enter a valid CPF')
+        return req.body
+      })
+      .required(),
+      birthday: Joi.date().format('DD/MM/YYYY').required(),
+      email: Joi.string().email().trim().required(),
+      password: Joi.string().trim().required(),
+      cep: Joi.string().trim().required(),
+      uf: Joi.string(),
+      city: Joi.string(),
+      address: Joi.string(),
+      number: Joi.number().required(),
+      complement: Joi.string(),
+      neighborhood: Joi.string(),
+    });
+
+    const { error } = await client.validate(req.body, {abortEarly: false})
+
+    if (error) throw error
+
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      errors: error.details.map((detail) => ({
+        name: detail.path.join('.'),
+        description: detail.message
+      }))
+    })
+  }
+}
--- src/routes/client.router.ts
+++ src/routes/client.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import ClientController from '../app/controller/ClientController';
+import validateClient from '../app/validations/client/validateClient';
 
 const router = Router()
 
-router.post('/api/v1/client', ClientController.create)
+router.post('/api/v1/client',validateClient , ClientController.create)
 router.get('/api/v1/client', ClientController.findAll)
 router.get('/api/v1/client/:id', ClientController.findById)
 router.delete('/api/v1/client/:id', ClientController.delete)

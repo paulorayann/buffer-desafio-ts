952278f372cd72bb8951c8f0dd4c2d15211e1a0b
paulorayann <paulorpsena@gmail.com>
2022-08-16 16:00:02 -0300

    feat: add sale Joi post method validation

---
+++ src/app/validations/sale/validateSale.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express'
+import JoiImport from 'joi'
+import DateExtension from '@joi/date'
+import {objectId}  from '../../utils/regex'
+
+const Joi = JoiImport.extend(DateExtension) as typeof JoiImport
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const client = Joi.object({
+      client: Joi.string().regex(objectId).message('Please enter a valid Client ID format').trim().required(),
+      clientCurrency: Joi.string().trim().required(),
+      date: Joi.date().format('DD/MM/YYYY').required(),
+      items: Joi.array().items(
+        Joi.object({
+          product: Joi.string().regex(objectId).message('Please enter a valid Product ID format').trim().required(),
+          qtd: Joi.number().required(),
+          unitValue: Joi.number().required(),
+        })
+      ).required(),
+      total: Joi.number(),
+      totalClient: Joi.number(),
+
+    });
+
+    const { error } = await client.validate(req.body, {abortEarly: false})
+
+    if (error) throw error
+
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      errors: error.details.map((detail) => ({
+        name: detail.path.join('.'),
+        description: detail.message
+      }))
+    })
+  }
+}
--- src/routes/sale.router.ts
+++ src/routes/sale.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import SaleController from '../app/controller/SaleController';
+import validateSale from '../app/validations/sale/validateSale';
 
 const router = Router()
 
-router.post('/api/v1/sale', SaleController.create)
+router.post('/api/v1/sale', validateSale, SaleController.create)
 router.get('/api/v1/sale', SaleController.findAll)
 router.get('/api/v1/sale/:id', SaleController.findById)
 router.delete('/api/v1/sale/:id', SaleController.delete)

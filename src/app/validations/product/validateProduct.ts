1c350e3637fc914699787aaae6f3d64507a6f91a
paulorayann <paulorpsena@gmail.com>
2022-08-16 15:59:45 -0300

    feat: add product Joi post method validation

---
+++ src/app/validations/product/validateProduct.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express'
+import Joi from 'joi'
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const client = Joi.object({
+      name: Joi.string().trim().required(),
+      category: Joi.string().trim().required(),
+      currency: Joi.string().trim().required(),
+      price: Joi.string().trim().required(),
+
+    });
+
+    const { error } = await client.validate(req.body, {abortEarly: false})
+
+    if (error) throw error
+
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      errors: error.details.map((detail) => ({
+        name: detail.path.join('.'),
+        description: detail.message
+      }))
+    })
+  }
+}
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import ProductController from '../app/controller/ProductController';
+import validateProduct from '../app/validations/product/validateProduct';
 
 const router = Router()
 
-router.post('/api/v1/product', ProductController.create)
+router.post('/api/v1/product', validateProduct, ProductController.create)
 router.get('/api/v1/product', ProductController.findAll)
 router.get('/api/v1/product/:id', ProductController.findById)
 router.delete('/api/v1/product/:id', ProductController.delete)

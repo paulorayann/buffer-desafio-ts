2c5c5477f18d5310891fe6c5078e8803730edb01
paulorayann <paulorpsena@gmail.com>
2022-08-16 23:56:07 -0300

    feat: add client password encryption

--- src/app/schema/ClientSchema.ts
+++ src/app/schema/ClientSchema.ts
@@ -1,14 +1,27 @@
 import mongoose, { Schema } from 'mongoose'
 import { IClient } from '../interfaces/ClientInterface'
+import bcryptjs from 'bcryptjs'
+
 
 const clientSchema = new Schema<IClient>({
-  name: { type: String, required: true },
-  cpf: { type: String, required: true },
-  birthday: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  cep: { type: String, required: true },
+  name: { type: String, trim: true, required: true },
+  cpf: { type: String, trim: true, required: true },
+  birthday: { type: Date, trim: true, required: true },
+  email: { type: String, lowercase: true, trim: true, required: true},
+  password: { type: String, required: true, select: false},
+  cep: { type: String, trim: true, required: true },
+  uf: { type: String },
+  city: { type: String },
+  address: { type: String, },
+  number: { type: String, trim: true, required: true },
+  complement: { type: String },
+  neighborhood: { type: String },
 })
 
-const Client = mongoose.model<IClient>('User', clientSchema)
+clientSchema.pre('save', async function pass(next){
+  const hash = await bcryptjs.hash(this.password, 10)
+  this.password = hash
+  next()
+})
+const Client = mongoose.model<IClient>('Client', clientSchema)
 export default Client

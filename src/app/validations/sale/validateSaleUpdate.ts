8b6f86567430ad642a56872066a7ffca4062e740
paulorayann <paulorpsena@gmail.com>
2022-08-17 11:52:04 -0300

    feat: add sale put validation

---
+++ src/app/validations/sale/validateSaleUpdate.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express'
+import JoiImport from 'joi'
+import DateExtension from '@joi/date'
+import {objectId}  from '../../utils/regex'
+
+const Joi = JoiImport.extend(DateExtension) as typeof JoiImport
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const client = Joi.object({
+      client: Joi.string().regex(objectId).message('Please enter a valid Client ID format').trim(),
+      clientCurrency: Joi.string().trim(),
+      date: Joi.date().format('DD/MM/YYYY'),
+      items: Joi.array().items(
+        Joi.object({
+          product: Joi.string().regex(objectId).message('Please enter a valid Product ID format').trim(),
+          qtd: Joi.number(),
+          unitValue: Joi.number(),
+        })
+      ),
+      total: Joi.number(),
+      totalClient: Joi.number(),
+    });
+
+    const { error } = await client.validate(req.body, {abortEarly: false})
+
+    if (error) throw error
+
+    return next();
+  } catch (error) {
+    return res.status(400).json({
+      errors: error.details.map((detail) => ({
+        name: detail.path.join('.'),
+        description: detail.message
+      }))
+    })
+  }
+}

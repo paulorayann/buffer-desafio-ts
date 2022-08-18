acf17bd46792e84291266ad45d2e63bd7cc05697
paulorayann <paulorpsena@gmail.com>
2022-08-18 20:28:37 -0300

    feat: add currency quote api consume

---
+++ src/app/utils/currencyQuoteAPI/CurrencyValue.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+class CurrencyValue {
+  async getCurrencyValue(currency:string, clientCurrency:string): Promise<number> {
+    const response = await axios.get(`https://economia.awesomeapi.com.br/${currency}-${clientCurrency}`)
+    return response.data
+  }
+}
+
+export default new CurrencyValue()

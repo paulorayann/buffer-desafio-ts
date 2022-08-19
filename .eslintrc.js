1723aa3aa76fb99fc52643cbfec75b9f332f0022
paulorayann <paulorpsena@gmail.com>
2022-08-19 10:43:54 -0300

    chore: add eslint and prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "extends": [
-        'plugin:@typescript-eslint/recommended',
-        "prettier"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    es2021: true,
+    node: true,
+    browser: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    camelcase: 'off',
+    'no-console': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-underscore-dangle': 'off'
+  }
+};

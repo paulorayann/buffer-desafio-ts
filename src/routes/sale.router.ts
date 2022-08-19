1ee7e18016de5bf619fbe08b1d7f5ee5f04822ae
paulorayann <paulorpsena@gmail.com>
2022-08-19 09:28:39 -0300

    feat: add sale get validation

--- src/routes/sale.router.ts
+++ src/routes/sale.router.ts
@@ -2,13 +2,15 @@ import { Router } from 'express'
 import SaleController from '../app/controller/SaleController'
 import validateSale from '../app/validations/sale/validateSale'
 import validateSaleUpdate from '../app/validations/sale/validateSaleUpdate'
+import validateSaleGet from '../app/validations/sale/validateSaleGet'
+import validateId from '../app/validations/validateId'
 
 const router = Router()
 
 router.post('/api/v1/sale', validateSale, SaleController.create)
-router.get('/api/v1/sale', SaleController.findAll)
-router.get('/api/v1/sale/:id', SaleController.findById)
-router.delete('/api/v1/sale/:id', SaleController.delete)
-router.put('/api/v1/sale/:id', validateSaleUpdate, SaleController.update)
+router.get('/api/v1/sale', validateSaleGet, SaleController.findAll)
+router.get('/api/v1/sale/:id', validateId, validateSaleGet, SaleController.findById)
+router.delete('/api/v1/sale/:id', validateId, SaleController.delete)
+router.put('/api/v1/sale/:id', validateId, validateSaleUpdate, SaleController.update)
 
 export default router

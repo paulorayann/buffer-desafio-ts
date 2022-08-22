371e71927d905bab8fd317c8c9586fcc08b005e5
paulorayann <paulorpsena@gmail.com>
2022-08-22 11:10:52 -0300

    feat: add custom error messages folder

---
+++ src/app/utils/customMessages.ts
@@ -0,0 +1,3 @@
+export const invalidCpfMessage = 'The CPF field has an invalid format, please try XXX.XXX.XXX-XX and use numbers only';
+export const invalidCepMessage = 'The CEP field has an invalid format, please try XXXXX-XXX';
+export const invalidObjectIdMessage = 'Please enter a valid ID';

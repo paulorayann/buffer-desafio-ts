71ba224d9fa71a90ae79e54ef0d56768c3bbbfd4
paulorayann <paulorpsena@gmail.com>
2022-08-22 15:47:39 -0300

    feat: add morgan logs and log path to access.log

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import cors from 'cors';
 import express from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 import routes from './routes/index.router';
 import './infra/database/mongo/index';
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 class App {
   public express: express.Application;
 
@@ -20,6 +24,11 @@ class App {
     this.express.use(express.json());
     this.express.use(express.urlencoded({ extended: true }));
     this.express.use(cors());
+    this.express.use(
+      morgan(' [:date[clf]] ":method - :url" :status :res[content-length] ":referrer" ":user-agent"', {
+        stream: accessLogStream
+      })
+    );
   }
 
   private routes(): void {

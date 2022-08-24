5e6918b35a27be747a611e0298887ac4ac772908
paulorayann <paulorpsena@gmail.com>
2022-08-24 11:45:37 -0300

    fix: birthday with unique parameter

--- src/app/schema/ClientSchema.ts
+++ src/app/schema/ClientSchema.ts
@@ -5,8 +5,8 @@ import bcryptjs from 'bcryptjs';
 
 const clientSchema = new Schema<IClient>({
   name: { type: String, trim: true, required: true },
-  cpf: { type: String, trim: true, required: true },
-  birthday: { type: Date, trim: true, unique: true, required: true },
+  cpf: { type: String, trim: true, unique: true, required: true },
+  birthday: { type: Date, trim: true, required: true },
   email: { type: String, lowercase: true, trim: true, unique: true, required: true },
   password: { type: String, required: true, select: false },
   cep: { type: String, trim: true, required: true },

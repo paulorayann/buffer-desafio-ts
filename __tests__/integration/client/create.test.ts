204f298bf8794a406a678bd444fbc3978b9086eb
paulorayann <paulorpsena@gmail.com>
2022-08-25 10:43:41 -0300

    test: add client create method integration test

---
+++ __tests__/integration/client/create.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import App from '../../../src/app';
+import faker from 'faker-br';
+
+jest.setTimeout(100000);
+
+it('should be able to create a new Client', async () => {
+  const createClient = await request(App).post('/api/v1/client').send({
+    name: faker.name.firstName(),
+    cpf: faker.br.cpf(),
+    birthday: '09/05/2003',
+    email: faker.internet.email(),
+    password: '12345678',
+    cep: '66640-677',
+    number: '23'
+  });
+  expect(createClient.status).toBe(201);
+});
+
+it('should not be able to create a new Client with empty required fields', async () => {
+  const createClient = await request(App).post('/api/v1/client').send({
+    name: '',
+    cpf: faker.br.cpf(),
+    birthday: '09/09/1990',
+    email: '',
+    password: '',
+    cep: '66640-677',
+    number: 23
+  });
+  expect(createClient.status).toBe(400);
+});
+
+it('should not be able to create a new Client with invalid cpf', async () => {
+  const createClient = await request(App).post('/api/v1/client').send({
+    name: faker.name.firstName(),
+    cpf: '111.222.345-67',
+    birthday: '09/09/1990',
+    email: faker.internet.email(),
+    password: '12345678',
+    cep: '66640-677',
+    number: 23
+  });
+  expect(createClient.status).toBe(400);
+});

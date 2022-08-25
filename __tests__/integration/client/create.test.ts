e746c22f6a7e748e539f1e049250982f7c2dbd6e
paulorayann <paulorpsena@gmail.com>
2022-08-25 14:14:37 -0300

    test: add client create method invalid cep and invalid email tests

--- __tests__/integration/client/create.test.ts
+++ __tests__/integration/client/create.test.ts
@@ -42,3 +42,29 @@ it('should not be able to create a new Client with invalid cpf', async () => {
   });
   expect(createClient.status).toBe(400);
 });
+
+it('should not be able to create a new Client with invalid CEP', async () => {
+  const createClient = await request(App).post('/api/v1/client').send({
+    name: faker.name.firstName(),
+    cpf: faker.br.cpf(),
+    birthday: '09/05/2003',
+    email: faker.internet.email(),
+    password: '12345678',
+    cep: '677',
+    number: '23'
+  });
+  expect(createClient.status).toBe(400);
+});
+
+it('should not be able to create a new Client with invalid e-mail', async () => {
+  const createClient = await request(App).post('/api/v1/client').send({
+    name: faker.name.firstName(),
+    cpf: faker.br.cpf(),
+    birthday: '09/09/1990',
+    email: 'teste.mail',
+    password: '12345678',
+    cep: '66640-677',
+    number: 23
+  });
+  expect(createClient.status).toBe(400);
+});

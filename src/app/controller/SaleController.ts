33b8fcd6515d01734d323373ae3772b2dec495cc
paulorayann <paulorpsena@gmail.com>
2022-08-25 23:53:51 -0300

    fix: sale delete method status code

--- src/app/controller/SaleController.ts
+++ src/app/controller/SaleController.ts
@@ -67,7 +67,7 @@ class SaleController {
       return res.status(204).send();
     } catch (error) {
       logger.log('error', 'Error deleting sale');
-      return res.status(400).json(error);
+      return res.status(500).json(error);
     }
   }
 }
